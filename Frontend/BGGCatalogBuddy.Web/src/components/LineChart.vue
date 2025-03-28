72607f4521d02cdd3db0d93ed2cb8bd4e6e87a1c
Darien Goodwin-Madden <91464820+DarienGoodwin@users.noreply.github.com>
2025-03-28 14:55:52 +0000

    Charts now change when theme swaps

--- Frontend/BGGCatalogBuddy.Web/src/components/LineChart.vue
+++ Frontend/BGGCatalogBuddy.Web/src/components/LineChart.vue
@@ -1,19 +1,20 @@
 <template>
-    <Line :data="data" :options="options" :style="style"></Line>
+  <Line :data="data" :options="mergedOptions" :style="style"></Line>
 </template>
 
 <script>
+import { useTheme } from 'vuetify';
 import { Line } from 'vue-chartjs';
 import {
-    Chart as ChartJS,
-    Title,
-    Tooltip,
-    Legend,
-    LineElement,
-    LinearScale,
-    PointElement,
-    CategoryScale,
-    Filler
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  LineElement,
+  LinearScale,
+  PointElement,
+  CategoryScale,
+  Filler
 } from 'chart.js';
 ChartJS.register(Title, Tooltip, Legend, LineElement, LinearScale, PointElement, CategoryScale, Filler);
 
@@ -24,6 +25,79 @@ export default {
     data: Object,
     options: Object,
     style: Object,
+  },
+  data: function () {
+    return {
+      theme: useTheme(),
+      chartColors: null
+    }
+  },
+  mounted() {
+    this.updateChartColors();
+  },
+  methods: {
+    getCssVariable(variable) {
+      return getComputedStyle(document.documentElement).getPropertyValue(variable).trim();
+    },
+    updateChartColors() {
+      this.chartColors = {
+        labelColor: `rgb(${this.getCssVariable('--v-theme-surface-darker-text')})`,
+        xTickColor: `rgb(${this.getCssVariable('--v-theme-surface-darker-text')})`,
+        xGridColor: `rgb(${this.getCssVariable('--v-theme-surface-light')})`,
+        yTickColor: `rgb(${this.getCssVariable('--v-theme-surface-darker-text')})`,
+        yGridColor: `rgb(${this.getCssVariable('--v-theme-surface-light')})`,
+      }
+    }
+  },
+  computed: {
+    mergedOptions() {
+      return this.$lodash.merge({}, this.globalOptions, this.options);
+    },
+    globalOptions() {
+      return {
+        plugins: {
+          legend: {
+            labels: {
+              color: this.chartColors.labelColor
+            }
+          }
+        },
+        scales: {
+          x: {
+            ticks: {
+              color: this.chartColors.xTickColor
+            },
+            grid: {
+              color: this.chartColors.xGridColor,
+            },
+            border: {
+              width: 3,
+              color: this.chartColors.xGridColor
+            },
+          },
+          y: {
+            ticks: {
+              color: this.chartColors.yTickColor
+            },
+            grid: {
+              color: this.chartColors.yGridColor,
+            },
+            border: {
+              width: 3,
+              color: this.chartColors.yGridColor
+            },
+          }
+        }
+      }
+    },
+  },
+  watch: {
+    'theme.global.name': {
+      immediate: true,
+      handler() {
+        this.updateChartColors();
+      }
+    }
   }
 };
 </script>
--- Frontend/BGGCatalogBuddy.Web/src/components/RadarChart.vue
+++ Frontend/BGGCatalogBuddy.Web/src/components/RadarChart.vue
@@ -1,10 +1,11 @@
 <template>
-  <Radar :data="data" :options="options"></Radar>
+  <Radar :data="data" :options="mergedOptions"></Radar>
 </template>
 
 <script>
 import { Chart as ChartJS, RadialLinearScale, CategoryScale, Title, Tooltip, Legend, PointElement, LineElement, Filler } from "chart.js";
 import { Radar } from 'vue-chartjs'
+import { useTheme } from 'vuetify';
 ChartJS.register(RadialLinearScale, CategoryScale, Title, Tooltip, Legend, PointElement, LineElement, Filler);
 
 export default {
@@ -13,6 +14,70 @@ export default {
   props: {
     data: Object,
     options: Object
+  },
+  data: function () {
+    return {
+      theme: useTheme(),
+      chartColors: null
+    }
+  },
+  mounted() {
+    this.updateChartColors();
+  },
+  methods: {
+    getCssVariable(variable) {
+      return getComputedStyle(document.documentElement).getPropertyValue(variable).trim();
+    },
+    updateChartColors() {
+      this.chartColors = {
+        labelColor: `rgb(${this.getCssVariable('--v-theme-surface-darker-text')})`,
+        angleLinesColor: `rgb(${this.getCssVariable('--v-theme-surface-light')})`,
+        gridColor: `rgb(${this.getCssVariable('--v-theme-surface-light')})`,
+        pointLabelColor: `rgb(${this.getCssVariable('--v-theme-surface-darker-text')})`,
+        tickColor: `rgb(${this.getCssVariable('--v-theme-surface-darker-text')})`,
+      }
+    }
+  },
+  computed: {
+    mergedOptions() {
+      return this.$lodash.merge({}, this.globalOptions, this.options);
+    },
+    globalOptions() {
+      return {
+        plugins: {
+          legend: {
+            labels: {
+              color: this.chartColors.labelColor,
+            }
+          }
+        },
+        scales: {
+          r: {
+            angleLines: {
+              color: this.chartColors.angleLinesColor
+            },
+            grid: {
+              color: this.chartColors.gridColor
+            },
+            pointLabels: {
+              color: this.chartColors.pointLabelColor
+            },
+            ticks: {
+              color: this.chartColors.tickColor,
+              backdropColor: 'transparent'
+            }
+          }
+        }
+      };
+    }
+  },
+  watch: {
+    'theme.global.name': {
+      immediate: true,
+      handler() {
+        this.updateChartColors();
+      }
+    }
   }
 };
 </script>
